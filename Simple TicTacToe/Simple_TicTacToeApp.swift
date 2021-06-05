c5d4b348a5415c9eaddc4f74105d9c369b8152c5
danang wijaya <danangwijaya750@gmail.com>
2021-06-05 22:16:42 +0700

    Initial Commit

---
+++ Simple TicTacToe/Simple_TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_TicTacToeApp.swift
+//  Simple TicTacToe
+//
+//  Created by Danang Wijaya on 05/06/21.
+//
+
+import SwiftUI
+
+@main
+struct Simple_TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
